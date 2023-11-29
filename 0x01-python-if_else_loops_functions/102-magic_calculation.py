ee37cb008683596d0be0087f813b105c784b02cf
Mohammed Elgaily <mbajily@gmail.com>
2023-11-28 18:02:44 -0800

    Write the Python function def magic_calculation(a, b, c): that does exactly the same as the following Python bytecode:

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    if a == 1 and b == 2 and c ==3:
+    if a == 1 and b == 2 and c == 3:
         return c
-    if a == 3 and b == 1 and c ==2:
+    if a == 3 and b == 1 and c == 2:
         return a + b
     if a == 30 and b == 10 and c == 2:
-        return a + b
+        return (a * 10) - c
     if a < b:
         if b > c:
             return a + b
