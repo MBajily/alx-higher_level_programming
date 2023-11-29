540afe75c6ec343b4aa3b50340e9583e786a5eeb
Mohammed Elgaily <mbajily@gmail.com>
2023-11-28 16:59:59 -0800

    handle empty input

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,7 +3,7 @@ def islower(c):
     string = ''
     for i in range(ord('a'), ord('z') + 1):
         string += chr(i)
-    if c in string:
+    if c in string and c:
         return True
     else:
         return False
