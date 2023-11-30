df70071ea4ff25f296575014b83a3594b3a59956
Mohammed Elgaily <mbajily@gmail.com>
2023-11-30 14:08:56 -0800

    Write the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    add = __import__('calculator_1').add
+    sub = __import__('calculator_1').sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 7):
+            c = add(c, i)
+            return c
+        else:
+            return sub(a, b)
