0915378c4615b94bb0f33c20b7edf7203307ff8f
Mohammed Elgaily <mbajily@gmail.com>
2023-12-04 15:11:51 -0800

    CPython is the reference implementation of the Python programming language. Written in C, CPython is the default and most widely used implementation of the language.

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <listobject.h>
+#include <Python.h>
+#include <object.h>
+
+
+/**
+ * print_python_list_info
+ *
+ * PyObject *p
+ */
+void print_python_list_info(PyObject *p)
+{
+	int i;
+	long int s = PyList_Size(p);
+
+	PyListObject *ob = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", s);
+	printf("[*] Allocated = %li\n", ob->allocated);
+	
+	for (i = 0; i < s; i++)
+	{
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+	}
+}
