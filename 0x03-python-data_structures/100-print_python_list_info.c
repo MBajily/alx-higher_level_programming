1df02b3489df1dac06c7b60dde98a2aa1c6b8df1
Mohammed Elgaily <mbajily@gmail.com>
2023-12-04 15:14:06 -0800

    Fix syntax errors.

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -20,6 +20,6 @@ void print_python_list_info(PyObject *p)
 	
 	for (i = 0; i < s; i++)
 	{
-		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+		printf("Element %i: %s\n", i, Py_TYPE(ob->ob_item[i])->tp_name);
 	}
 }
