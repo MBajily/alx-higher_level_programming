a9fba2935ee0491039d030636add8157ab7f7072
Mohammed Elgaily <mbajily@gmail.com>
2023-12-18 14:37:14 -0800

    Fix some compiling errors.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    try:
-        string = ''
-        while (i < x):
-            if str(my_list[i]).isdigit():
-                print("{:d}".format(my_list[i]), end="")
-            i += 1
-        print()
-        return i
-    except Exception as e:
-        print(e)
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count = i
+        except Exception as e:
+            continue
+    print()
+    return count
