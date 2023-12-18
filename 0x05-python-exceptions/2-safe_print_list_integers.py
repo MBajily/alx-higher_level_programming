e59080800790b1789dc674c9a16a0de1596b29f2
Mohammed Elgaily <mbajily@gmail.com>
2023-12-18 14:38:40 -0800

    Fix some compiling errors.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            count = i
+            count += 1
         except Exception as e:
             continue
     print()
