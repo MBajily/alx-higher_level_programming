7d960e521c6fae59a88af3b8f728adf28dc89848
Mohammed Elgaily <mbajily@gmail.com>
2023-12-18 14:49:07 -0800

    Fix Betty styling.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    """a function that prints the first x elements of a list and only integers.
+
+    Args:
+        my_list (list): list of elements.
+        x (int): The number of elements.
+
+    Returns:
+        The number of elements printed.
+    """
     count = 0
     for i in range(x):
         try:
