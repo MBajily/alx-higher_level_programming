f8f7b66e4d03ca3309ce2b0e81e15e0c08d9a43d
Mohammed Elgaily <mbajily@gmail.com>
2023-12-18 02:52:42 -0800

    Fix Betty Styling.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
@@ -8,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             i += 1
         print()
-        return (i)
+        return i
         
     except:
         print()
-        return (i)
+        return i
