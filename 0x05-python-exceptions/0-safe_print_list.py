64c5c98afdce7fd4940c531d7c025be9e0758bfb
Mohammed Elgaily <mbajily@gmail.com>
2023-12-18 14:21:42 -0800

    Use print format method to get the full score.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         string = ''
         while (i < x):
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
             i += 1
         print()
         return i
