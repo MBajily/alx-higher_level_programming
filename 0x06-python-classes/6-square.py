3c1b9c050b7ccea2485789a27567e6337b0228f3
Mohammed Elgaily <mbajily@gmail.com>
2023-12-19 15:57:45 -0800

    Merge the if conditions.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,7 +36,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if ((len(value) != 2) or (not isinstance(value, tuple)) or (not all(isinstance(num, int) for num in value)) or (not all(num >= 0 for num in value))):
+        if ((len(value) != 2) or (not isinstance(value, tuple)) or
+                (not all(isinstance(num, int) for num in value)) or
+                (not all(num >= 0 for num in value))):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__posistion = value
 
