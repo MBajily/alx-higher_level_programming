57313c2caa8b5ab2b6b210631e974a1f70bac6eb
Mohammed Elgaily <mbajily@gmail.com>
2023-12-19 12:06:25 -0800

    Empty class.

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
+
 class Square:
-    """an empty class Square that defines a square.
-    
-    Returns:
-        - None
-    """
+    """an empty class Square that defines a square."""
     pass
