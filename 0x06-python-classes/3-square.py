4dac2febad2087e0474e5746228677f01b03ce5e
Mohammed Elgaily <mbajily@gmail.com>
2023-12-19 12:34:34 -0800

    Write a class Square that defines a square by: (based on 2-square.py)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """an empty class Square that defines a square."""
+
+    def __init__(self, size=0):
+        """Constructor
+
+        Args:
+            size: square size.
+
+        Raises:
+            ValueError: "size must be >= 0"
+            TypeErro: ("size must be an integer"
+        """
+        if isinstance(size, int):
+            if int(size) < 0:
+                raise ValueError("size must be >= 0")
+
+        else:
+            raise TypeError("size must be an integer")
+        self.__size = size
+
+    def area(self):
+        """Area
+
+        Returns:
+            size: the square's size.
+        """
+        return self.__size
