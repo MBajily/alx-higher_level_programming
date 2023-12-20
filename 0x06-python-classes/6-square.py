05f39fc1a0ec4c5635dcc5e738bb42954edc9501
Mohammed Elgaily <mbajily@gmail.com>
2023-12-19 16:00:32 -0800

    Merge the if conditions.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,13 +51,11 @@ class Square:
         if self.__size == 0:
             print("")
             return
-
-        else:
-            for i in range(self.__position[1]):
-                print("")
-            for i in range(self.__size):
-                for k in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print("")
+        for i in range(self.__position[1]):
+            print("")
+        for i in range(self.__size):
+            for k in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
