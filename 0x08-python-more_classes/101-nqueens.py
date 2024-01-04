082c9d5796d7d7cfb16f1e722de6e8ace39d24f7
Mohammed Elgaily <48401371+MBajily@users.noreply.github.com>
2024-01-04 23:58:51 +0300

    The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. Write a program that solves the N queens problem.

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+"""Solves the N-queens puzzle.
+"""
+import sys
+
+
+def get_s(chessboard):
+    """Return lists representation of a solved chessboard."""
+    s = []
+    for r in range(len(chessboard)):
+        for c in range(len(chessboard)):
+            if chessboard[r][c] == "Q":
+                s.append([r, c])
+                break
+
+    return (s)
+
+def init_chessboard(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    chessboard = []
+    for i in range(n):
+        chessboard.append([])
+
+    [row.append(' ') for i in range(n) for row in chessboard]
+    return (chessboard)
+
+
+def chessboard_deepcopy(chessboard):
+    """Return a deepcopy"""
+    if isinstance(chessboard, list):
+        return list(map(chessboard_deepcopy, chessboard))
+
+    return (chessboard)
+
+
+def xout(chessboard, row, column):
+    """X out spots on a chessboard.
+
+    Args:
+        chessboard (list): current chessboard.
+        row (int): The row where a queen was last played.
+        column (int): The colum where a queen was last played.
+    """
+    for c in range(column + 1, len(chessboard)):
+        chessboard[row][c] = "x"
+
+    for c in range(column - 1, -1, -1):
+        chessboard[row][c] = "x"
+
+    for r in range(row + 1, len(chessboard)):
+        chessboard[r][column] = "x"
+
+    for r in range(row - 1, -1, -1):
+        chessboard[r][column] = "x"
+
+    c = column + 1
+    for r in range(row + 1, len(chessboard)):
+        if c >= len(chessboard):
+            break
+        chessboard[r][c] = "x"
+        c += 1
+
+    c = column - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        chessboard[r][c]
+        c -= 1
+
+    c = column + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(chessboard):
+            break
+        chessboard[r][c] = "x"
+        c += 1
+
+    c = column - 1
+    for r in range(row + 1, len(chessboard)):
+        if c < 0:
+            break
+        chessboard[r][c] = "x"
+        c -= 1
+
+
+def rec_solve(chessboard, row, queens, solutions):
+    """solve an N-queens puzzle.
+
+    Args:
+        chessboard (list): current chessboard.
+        row (int): current row.
+        queens (int): current place of queens.
+        s (list): lists of solutions.
+    Returns:
+        s
+    """
+    if queens == len(chessboard):
+        s.append(get_s(chessboard))
+        return (s)
+
+    for c in range(len(chessboard)):
+        if chessboard[row][c] == " ":
+            tmp_chessboard = chessboard_deepcopy(chessboard)
+            tmp_chessboard[row][c] = "Q"
+            xout(tmp_chessboard, row, c)
+            s = rec_solve(tmp_chessboard, row + 1,
+                                        queens + 1, s)
+
+    return (s)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    chessboard = init_chessboard(int(sys.argv[1]))
+    solutions = rec_solve(chessboard, 0, 0, [])
+    for sol in solutions:
+        print(sol)
