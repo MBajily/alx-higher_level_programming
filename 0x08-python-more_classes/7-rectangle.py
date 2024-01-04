2588ec3f9d8e984fad97bb6e76b120f8cae46259
Mohammed Elgaily <mbajily@gmail.com>
2024-01-04 07:39:27 -0800

    Convert any print_symbol value to str.

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
         if self.__height != 0 and self.__width != 0:
             for i in range(self.__height):
                 for j in range(self.__width):
-                    string += self.print_symbol
+                    string += str(self.print_symbol)
                 if i != (self.__height - 1):
                     string += '\n'
         return string
