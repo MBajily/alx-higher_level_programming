e25ba52a8319aa05ab3f3e979bb899c2114083c8
Mohammed Elgaily <mbajily@gmail.com>
2024-01-03 16:22:17 -0800

    Create a python file for say_my_name function and write a testing doc for this function.

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,13 +6,17 @@ def say_my_name(first_name, last_name=""):
     """Print a name.
 
     Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
+        first_name (str): first name.
+        last_name (str): last name.
     Raises:
-        TypeError: If either of first_name or last_name are not strings.
+        TypeError: If first_name or last_name are not strings.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+
+    result = "My name is {} {}".format(first_name, last_name)
+
+    print(result)
