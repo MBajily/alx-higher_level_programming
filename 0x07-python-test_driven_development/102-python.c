243e0d815152aa88e89e3d0f18b66f7550d33779
Mohammed Elgaily <mbajily@gmail.com>
2024-01-03 17:07:03 -0800

    Create a function that prints Python strings.

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,4 +1,9 @@
 #include "Python.h"
+#include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
+#include <Python.h>
+#include <unicodeobject.h>
 
 /**
  * print_python_string - Prints information about Python strings.
@@ -10,19 +15,23 @@ void print_python_string(PyObject *p)
 
 	fflush(stdout);
 
-	printf("[.] string object info\n");
+	wprintf(L"[.] string object info\n");
 	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
-		printf("  [ERROR] Invalid String Object\n");
+		wprintf(L"  [ERROR] Invalid String Object\n");
 		return;
 	}
 
 	length = ((PyASCIIObject *)(p))->length;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
+	{
+		wprintf(L"  type: compact ascii\n");
+	}
 	else
-		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+	{
+		wprintf(L"  type: compact unicode object\n");
+	}
+	wprintf(L"  length: %lu\n", length);
+	wprintf(L"  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
