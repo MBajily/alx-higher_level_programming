ef883264938ab90a3c272a35433c6d5ee4c754d4
Mohammed Elgaily <mbajily@gmail.com>
2024-01-03 16:28:52 -0800

    Fix the errors in print_square function.

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,19 +3,21 @@
 
 
 def print_square(size):
-    """Print a square with the # character.
+    """Print a square using '#'.
 
     Args:
-        size (int): The height/width of the square.
+        size (int): size(height) x size(width)
     Raises:
         TypeError: If size is not an integer.
         ValueError: If size is < 0
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    if size < 0:
+
+    if (size < 0):
         raise ValueError("size must be >= 0")
 
     for i in range(size):
-        [print("#", end="") for j in range(size)]
+        for j in range(size)
+            print("#", end="")
         print("")
