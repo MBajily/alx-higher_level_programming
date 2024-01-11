53ea1876462599fe725d10f147fdf2158c8a86f7
Mohammed Elgaily <48401371+MBajily@users.noreply.github.com>
2024-01-12 02:43:41 +0300

    Fix the if condition.

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -11,7 +11,7 @@ def is_same_class(obj, a_class):
     Returns:
         True or False.
     """
-    if isinstance(obj, a_class):
+    if a_class == type(obj):
         return True
 
     return False
