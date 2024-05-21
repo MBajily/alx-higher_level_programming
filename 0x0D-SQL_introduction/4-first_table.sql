1762127a800b9cc2c70082fda3dfe4cce6dfe25c
Mohammed Elgaily <mbajily@gmail.com>
2024-05-21 15:09:35 +0300

    Fix the sytax error.

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
--- creates a table called first_table
 -- 'first_table' {id INT} and {name VARCHAR(256)}
-CREATE TABLE IF NOT EXISTS `first_table` {`id` INT, `name` VARCHAR(256)};
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
