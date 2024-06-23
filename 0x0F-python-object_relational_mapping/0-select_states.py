c3c2035d80761d0b09c310088d2940b521c495d5
Mohammed Elgaily <48401371+MBajily@users.noreply.github.com>
2024-06-23 20:19:23 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,14 +5,12 @@ import sys
 
 def list_states():
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-    passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
-    
     for row in rows:
         print(row)
-    
     cur.close()
     db.close()
 
