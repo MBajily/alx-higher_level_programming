02e9f79065187eaf5eae186d4f5e4f1ff171779c
root <mbajily@gmail.com>
2024-07-04 04:36:46 -0700

    update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,30 +7,30 @@ def find_peak(list_of_integers):
     n = len(list_of_integers)
 
     # Check if the list is empty
-    if n == 0:
+    if n == 0 or list_of_integers is None:
         return None
 
     # Check if the list has only one element
-    if n == 1:
-        return list_of_integers[0]
+    #if n == 1:
+    #    return list_of_integers[0]
 
     # Binary search to find the peak
     low = 0
-    high = n - 1
-    mid = ((low + high) // 2) + low
-
-    if high == 0:
-        return list_of_integers[0]
+    high = n
+    mid = int(((low + high) // 2) + low)
 
     if high == 1:
+        return list_of_integers[0]
+
+    if high == 2:
         return max(list_of_integers)
 
     if list_of_integers[mid] >= list_of_integers[mid - 1]\
             and list_of_integers[mid] >= list_of_integers[mid + 1]:
-                return list_of_integers[mid]
+        return list_of_integers[mid]
 
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid + 1:])
+        return find_peak(list_of_integers[mid:])
 
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
