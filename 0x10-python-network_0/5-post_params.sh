1c45bc277e18121a435d39d2f1567854942c2516
root <mbajily@gmail.com>
2024-07-04 04:05:47 -0700

    5. cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -10,7 +10,4 @@ email="test@gmail.com"
 subject="I will always be here for PLD"
 
 # Send a POST request to the URL with the parameters
-response=$(curl -s -X POST -d "email=$email&subject=$subject" "$1") 
-
-# Display the body of the response
-echo "$response"
+curl -s -X POST -d "email=$email&subject=$subject" "$1"
