55a824072f1471572fe78a97f842381fceaa6f78
Mohammed Elgaily <mbajily@gmail.com>
2024-07-05 16:50:28 +0300

    Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,16 @@
-"""a Python script that takes in a URL,
-sends a request to the URL and displays
-the body of the response (decoded in utf-8).
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
 """
 import sys
-from urllib.request import  urlopen
-import urllib.error
+from urllib import request, error
 
 
 if __name__ == "__main__":
     try:
-        with urlopen(sys.argv[1]) as response:
+        with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('UTF-8'))
 
-    except urllib.error.HTTPError as error:
+    except error.HTTPError as error:
         print('Error code:', error.code)
