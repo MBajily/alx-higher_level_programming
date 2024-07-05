83b3acac899a985212416c55f914847596a8cc37
Mohammed Elgaily <mbajily@gmail.com>
2024-07-05 16:21:34 +0300

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,7 @@
 
 if __name__ == '__main__':
     import urllib.request
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         response = response.read()
         print("Body response:")
